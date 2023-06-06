c22cf6bf6c5fee084e28909f7cf87e89ea384c59
EnriqueMendozaPrrfect <lionsixeight@gmail.com>
2023-06-05 23:46:49 -0600

    tutorial 1 and 2

---
+++ 1-hello-world/hello_world.dart
@@ -0,0 +1,15 @@
+void main() {
+  print("Hello world");
+
+  String x = "hello world";
+  print(x);
+
+  var y = 5;
+  print(y);
+
+  y = 6;
+  print(y);
+
+  var isValid = false;
+  print(isValid);
+}
---
+++ 2-operadores-aritmeticos/aritmetica.dart
@@ -0,0 +1,30 @@
+// () Parentesis
+// / y * División y multiplicación
+// + y -	Suma y resta
+// La forma en la que se resuelven es de izquierda a derecha
+
+void main () {
+  var suma = 5 + 6;
+  print('el valor de la suma es: $suma');
+
+  var resta = 5 - 2;
+  print('el valor de la resta es: $resta');
+
+  var multiplicacion = 3 * 4;
+  print('el valor de la multiplicacion es: $multiplicacion');
+
+  var divicion = 12 / 3;
+  print('el valor de la divicion es: $divicion');
+
+  var parentesis = 5 * (3 + 2);
+  print('el valor de la parentesis es: $parentesis');
+
+  parentesis = 5 + 3 * 2;
+  print('el valor de la parentesis es: $parentesis');
+}
+
+// Ejercicios
+// Calcula el área de un cuadrado de 8 e imprime el resultado
+// Calcula el área de un triangulo de base 5 y altura 7 e imprime el resultado
+// Calcula el área de un trapesio de lado superior es de 5 y el lado inferior 7 
+// con altura de 4 e imprime el resultado.
